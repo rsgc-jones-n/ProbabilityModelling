7c5632a85c6a78aea788df1341b1117fe57e7f35
Nick Jones <nickj918@hotmail.com>
2017-01-16 14:33:27 -0500

    Initial Commit

---
+++ ProbabilityModelling/ProbabilityModelling/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  ProbabilityModelling
+//
+//  Created by Nicholas Jones on 2017-01-16.
+//  Copyright © 2017 Nicholas Jones. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
