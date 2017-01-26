42f72e7f18a1842b0567d6a1db1093e13936296e
Nick Jones <nickj918@hotmail.com>
2017-01-26 09:31:22 -0500

    Need to add missing statements that make game playable to user

---
+++ ProbabilityModelling/ProbabilityModelling/userResponse.swift
@@ -0,0 +1,53 @@
+//
+//  userResponse.swift
+//  ProbabilityModelling
+//
+//  Created by Nicholas Jones on 2017-01-26.
+//  Copyright © 2017 Nicholas Jones. All rights reserved.
+//
+
+import Foundation
+
+class UserResponse {
+    
+    var response : Bool = false
+    var responseString = "X"
+    
+// Ask the player if they would like to hit, stand, double or split
+func question() {
+    
+    // Repeat until a proper response is given
+    repeat {
+        
+        print("Would you like to hit or stand?\n")
+        
+        if let input = readLine(strippingNewline: true) {
+            responseString = input
+            
+            print("")
+            
+            // If the player hits
+            if responseString == "hit" || responseString == "Hit" {
+                
+                Game.hit()
+                
+                //print("Player Hit")
+                response = true
+                
+                // Id the player stands
+            } else if responseString == "stand" || responseString == "Stand" {
+                
+                Game.stand()
+                
+                //print("Plater Stands")
+                response = false
+                
+            } else {
+                responseString = "X"
+            }
+        }
+    } while responseString == "X"
+    
+}
+
+}
